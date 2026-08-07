1ee4163e5a8128583b4b9bb2a0ef865013ca19f9
hisboye <hisboye2104@gmail.com>
2026-08-07 10:01:11 +0100

    configure test_app.py

--- test/test_app.py
+++ test/test_app.py
@@ -1,4 +1,10 @@
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
 from app import app
+
 
 def test_home_page():
     client = app.test_client()
